5caf29d23d2237caab80ec012170e6b09d3cb101
Julia <s02190029@gse.cs.msu.ru>
2022-02-27 19:59:12 +0300

    git push Everything up-to-date:   anna_.txt

---
+++ 20220221/1/fipsedixit.py
@@ -0,0 +1,29 @@
+import ipsedixit
+import os
+import sys
+
+defaults = ['caesar', 'tacitus']
+
+if len(sys.argv) < 3:
+    generator = ipsedixit.Generator()
+    paragraphs = generator.paragraphs(int(sys.argv[1]))
+    print('\n\n'.join(paragraphs))
+    
+elif sys.argv[2] in defaults or os.path.isfile(sys.argv[2]):
+    if sys.argv[2] in defaults:
+        text = sys.argv[2]
+        
+    elif os.path.isfile(sys.argv[2]):
+        try:
+            with open(sys.argv[2], 'rt') as word_base:
+                text = word_base.read()
+        except FileNotFoundError as E:
+            print("Error! This file does not exist")
+            sys.exit()
+    generator = ipsedixit.Generator(text)
+    sys.argv.pop(2)
+else:
+    generator = ipsedixit.Generator()
+args = ipsedixit.parse_args()
+
+print('\n\n'.join(generator.paragraphs(args.num, args.min, args.max)))
