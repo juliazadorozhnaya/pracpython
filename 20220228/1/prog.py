84ea3a0bea03d157faf61acd7b3f1620e6059c0e
Julia <s02190029@gse.cs.msu.ru>
2022-03-08 19:10:12 +0300

    2: 2/3

--- 20220228/1/prog.py
+++ 20220228/1/prog.py
@@ -1,11 +1,11 @@
 import textdistance
 
 
-def dist(s1, s2):
+def dist(s1, s2, s3):
     return textdistance.levenshtein(s1, s2)
 
 
 s1 = input()
 s2 = input()
 s3 = input()
-res = dist(s1, s2)
+res = dist(s1, s2, s3)
