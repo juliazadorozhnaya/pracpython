39919a045fb462b93c3c82f4d8b16cc76a2bcf29
Julia <s02190029@gse.cs.msu.ru>
2022-03-08 18:51:29 +0300

    1: 4/5

--- 20220228/1/prog.py
+++ 20220228/1/prog.py
@@ -12,6 +12,7 @@ def dist(s1, s2, s3):
 
 s1 = input()
 s2 = input()
+
 s3 = input()
 
 with mp.Pool(processes=1) as pool:
@@ -20,6 +21,4 @@ with mp.Pool(processes=1) as pool:
         print(process.get(timeout=1))
     except mp.context.TimeoutError:
         print(-1)
-
-
 
