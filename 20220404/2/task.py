6e515794d26e79f442d9d4f0838d3be18d4fbd9c
Julia <s02190029@gse.cs.msu.ru>
2022-04-10 20:43:59 +0300

    add new README

--- 20220404/2/task.py
+++ 20220404/2/task.py
@@ -1,11 +1,19 @@
 """
 Dummy module.
 """
+DUMMY_CONST = 100500
 
 class Dummy:
     def __init__(self, val):
             """
             Dummy init.
+            
+            :param val: Initial value
             """
-    self.init = val
-        
+            self.init = val
+    def incr(self, add):
+            """
+            Stringify me.
+            
+            :return: String
+            """
