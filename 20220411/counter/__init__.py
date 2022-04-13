2c9148aae9318178860336bc7c5e0178301225b7
Julia <s02190029@gse.cs.msu.ru>
2022-04-13 11:16:10 +0300

    Classwork

--- 20220411/counter/__init__.py
+++ 20220411/counter/__init__.py
@@ -1,10 +1,10 @@
 import gettext
 
 translation = gettext.translation("counter", "po", fallback=True)
-_, ngettext = translation.gettext, translation.ngettext
-
+_ = translation.gettext
+ngettext = translation.ngettext
 
 def dialog():
     while s := input(_("Input a string: ")):
         n = len(s.split())
-        print(ngettext("{} word entered", "{} words entered", n).format(n))
+        print(ngettext("{s} word entered", "{s} words entered", n).format(s=n))
