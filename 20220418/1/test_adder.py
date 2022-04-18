2c33e3e75fb542395253785ba87219985f6d4aaa
Julia <s02190029@gse.cs.msu.ru>
2022-04-18 13:37:45 +0300

    I was at the seminar

--- 20220418/1/test_adder.py
+++ 20220418/1/test_adder.py
@@ -1,13 +1,20 @@
 import unittest
 import adder
+import random
 
 class TestAdder(unittest.TestCase):
     
+    def setuUp(self):
+        self.A, self.B = random.randrange(0 ,100), random.randrange(0 , 100)
+        self.C = self.A + self.B
+        self.S, self.T = "QWE", "rty" 
+    
     def test_numbers(self):
-        self.unittest.asserEqual(adder.adder(100500, 42), 100542)
+        self.unittest.asserEqual(adder.adder(self.A, self.B), self.C)
         
     def test_strings(self):
-        self.assertEqual(adder.adder("QWE", "rty"), "QWErty")
+        self.assertEqual(adder.adder(self.S, self.T), "QWErty")
         
     def test_exception(self):
-        adder.adder(123)
+        with self.assertRaises(TypeError):
+            adder.adder(123)
